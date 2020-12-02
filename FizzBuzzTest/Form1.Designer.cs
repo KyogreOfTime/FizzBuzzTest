7902138c318220cc8dd264cb7451f7aee001d55f
KyogreOfTime <75351912+KyogreOfTime@users.noreply.github.com>
2020-12-02 00:05:13 -0500

    Initial commit

---
+++ FizzBuzzTest/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace FizzBuzzTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fzBzOutputLabel = new System.Windows.Forms.Label();
+            this.FizzButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // fzBzOutputLabel
+            // 
+            this.fzBzOutputLabel.Location = new System.Drawing.Point(59, 124);
+            this.fzBzOutputLabel.Name = "fzBzOutputLabel";
+            this.fzBzOutputLabel.Size = new System.Drawing.Size(673, 162);
+            this.fzBzOutputLabel.TabIndex = 0;
+            // 
+            // FizzButton
+            // 
+            this.FizzButton.Location = new System.Drawing.Point(253, 36);
+            this.FizzButton.Name = "FizzButton";
+            this.FizzButton.Size = new System.Drawing.Size(80, 32);
+            this.FizzButton.TabIndex = 1;
+            this.FizzButton.Text = "FizzBuzz!";
+            this.FizzButton.UseVisualStyleBackColor = true;
+            this.FizzButton.Click += new System.EventHandler(this.FizzButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(428, 36);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(75, 32);
+            this.ExitButton.TabIndex = 2;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 248);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.FizzButton);
+            this.Controls.Add(this.fzBzOutputLabel);
+            this.Name = "Form1";
+            this.Text = "Fizz Buzz Test";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label fzBzOutputLabel;
+        private System.Windows.Forms.Button FizzButton;
+        private System.Windows.Forms.Button ExitButton;
+    }
+}
+
---
+++ FizzBuzzTest/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FizzBuzzTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void FizzButton_Click(object sender, EventArgs e)
+        {
+            //declaring arrays to be filled with values and an int to help do said filling
+            int fzBzValue = 1;
+            int[] fzBzArray = new int[100];
+            string[] fzBzString = new string[100];
+            
+            //for loop to fill fzBzArray with numerical values
+            for(int fzBzIndex = 0; fzBzIndex < 100; fzBzIndex++)
+            {
+                fzBzArray[fzBzIndex] = fzBzValue;
+
+                fzBzValue++;
+            }
+            
+            //declaring int to help with the following for loop
+            int fzBzValue2 = 1;
+
+            //for loop to fill array fzBzString with string values
+            for(int fzBzIndex2 = 0; fzBzIndex2 < 100; fzBzIndex2++)
+            {
+                if (fzBzValue2 % 3 == 0 && fzBzValue2 % 5 == 0)
+                {
+                    fzBzString[fzBzIndex2] = "FizzBuzz";
+                }
+                else if (fzBzValue2 % 3 == 0)
+                {
+                    fzBzString[fzBzIndex2] = "Fizz";
+                }
+                else if (fzBzValue2 % 5 == 0)
+                {
+                    fzBzString[fzBzIndex2] = "Buzz";
+                }
+                else
+                {
+                    fzBzString[fzBzIndex2] = fzBzArray[fzBzIndex2].ToString();
+                }
+
+                fzBzValue2++;
+            }
+            //was inspired to use string.Join from
+            //https://stackoverflow.com/questions/26434458/how-to-print-the-contents-of-an-array-to-a-label-in-c-sharp
+            fzBzOutputLabel.Text = string.Join(", ", fzBzString);
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
